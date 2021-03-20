9777106773695a6c7355e5cf89caef99c0df32f1
Renzo <renzocs293@gmail.com>
2021-03-20 11:11:46 -0500

    first commit

---
+++ src/ComponentFilter.js
@@ -0,0 +1,44 @@
+import { html, css, LitElement } from 'lit-element';
+
+export class ComponentFilter extends LitElement {
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+        padding: 25px;
+        color: var(--component-filter-text-color, #000);
+      }
+      button{
+        background-color: blue;
+        color:  white;
+        width: 200px;
+        height: 30px;
+        border-radius: 5px;
+      }
+    `;
+  }
+
+  static get properties() {
+    return {
+      title: { type: String },
+      counter: { type: Number },
+    };
+  }
+
+  constructor() {
+    super();
+    this.title = 'Hey there';
+    this.counter = 1;
+  }
+
+  __increment() {
+    this.counter += 1;
+  }
+
+  render() {
+    return html`
+      <h2>${this.title} Nr. ${this.counter}!</h2>
+      <button @click=${this.__increment}>increment</button>
+    `;
+  }
+}
